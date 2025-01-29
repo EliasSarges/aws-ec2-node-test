e4e2d926ebe381ade0c4faf7f128750875d8d318
Elias Castro <38994457+EliasSarges@users.noreply.github.com>
2025-01-29 16:16:32 -0300

    feat: simple node api

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => console.log("Servidor iniciado na porta 3000"));
+
+app.get("/", (req, res) => res.send("me deve 50 centavos"));
